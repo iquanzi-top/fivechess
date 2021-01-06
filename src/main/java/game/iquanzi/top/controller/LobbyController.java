adba5ba8777e2f23a53a43cc04b8f2362fdce07c
Mr.Z <zeldady@aliyun.com>
2021-01-06 13:44:17 +0800

    动态创建gridpane

--- src/main/java/game/iquanzi/top/controller/LobbyController.java
+++ src/main/java/game/iquanzi/top/controller/LobbyController.java
@@ -6,22 +6,25 @@ import cn.hutool.core.util.StrUtil;
 import game.iquanzi.top.service.MessageService;
 import game.iquanzi.top.service.SessionService;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.TitledPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Pagination;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.URL;
 
-import static game.iquanzi.top.config.R.Lobby.*;
+import static game.iquanzi.top.config.R.Lobby.ID_PAGING;
 
 /**
  * 游戏大厅控制器<br/>
@@ -40,6 +43,8 @@ public class LobbyController extends Application {
     private Label wonNums;
     @FXML
     private Label lostNums;
+    @FXML
+    private Pagination paging;
 
     /**
      * 显示窗口
@@ -60,7 +65,7 @@ public class LobbyController extends Application {
         childs.forEach(child -> {
             String childId = child.getId();
             log.debug("一级组件ID：{}", childId);
-            if (StrUtil.isNotBlank(childId) && childId.equalsIgnoreCase(ID_PEERPANE)) {
+            /*if (StrUtil.isNotBlank(childId) && childId.equalsIgnoreCase(ID_PEERPANE)) {
                 ObservableList<Node> childrens = ((VBox) child).getChildren();
                 childrens.forEach(c -> {
                     String cId = c.getId();
@@ -88,6 +93,14 @@ public class LobbyController extends Application {
                         });
                     }
                 });
+            } else */
+            if (StrUtil.isNotBlank(childId) && childId.equalsIgnoreCase(ID_PAGING)) {
+                paging = (Pagination)child;
+                paging.setStyle("-fx-padding: 1, 0;");
+                paging.setPageFactory(pageIndex -> {
+                    // todo 创建表格，并返回
+                    return createGridPane(pageIndex);
+                });
             }
         });
 
@@ -100,12 +113,50 @@ public class LobbyController extends Application {
             log.debug("监听到程序窗口关闭事件");
             getChessSession().getClient().shutdown();
         });
-
-        loadData();
     }
 
-    private void loadData() {
-        MessageService.getInstance().getOnlineUsers(0);
+    private GridPane createGridPane(int pageIndex) {
+        GridPane gridPane = new GridPane();
+        gridPane.setGridLinesVisible(true);
+        gridPane.setPrefWidth(560);
+        gridPane.setPrefHeight(560);
+        gridPane.setAlignment(Pos.TOP_CENTER);
+
+        ObservableList<ColumnConstraints> columnConstraints = gridPane.getColumnConstraints();
+        ColumnConstraints cc = new ColumnConstraints();
+        cc.setHalignment(HPos.CENTER);
+        cc.setHgrow(Priority.SOMETIMES);
+        cc.setPrefWidth(180);
+        cc.setMaxWidth(180);
+        cc.setMinWidth(180);
+        columnConstraints.add(cc);
+        columnConstraints.add(cc);
+        columnConstraints.add(cc);
+
+        ObservableList<RowConstraints> rowConstraints = gridPane.getRowConstraints();
+
+        RowConstraints rc = new RowConstraints();
+        rc.setValignment(VPos.CENTER);
+        rc.setVgrow(Priority.SOMETIMES);
+        rc.setPrefHeight(180);
+        rc.setMaxHeight(180);
+        rc.setMinHeight(180);
+        rowConstraints.add(rc);
+        rowConstraints.add(rc);
+        rowConstraints.add(rc);
+
+        gridPane.setId("usersPane_" + pageIndex);
+
+        Platform.runLater(() -> loadData(pageIndex));
+        return gridPane;
+    }
+
+    /**
+     * 加载在线用户数据
+     */
+    private void loadData(int pageIndex) {
+        // 在消息响应时刷新UI层数据
+        MessageService.getInstance().getOnlineUsers(pageIndex);
     }
 
     private SessionService getChessSession() {
